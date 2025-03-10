c02c7335a1885da69bf45fa91c9e9ba9c8f55ff2
gissellenoriko481 <gissellenoriko481@gmail.com>
2025-03-10 04:00:34 +0000

    Add 5.cpp

---
+++ src/5.cpp
@@ -0,0 +1,11 @@
+// A simple C++ program to calculate the sum of two numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1 = 4;
+  int num2 = 5;
+  int sum = num1 + num2;
+  cout << "The sum is: " << sum << endl;
+  return 0;
+}
