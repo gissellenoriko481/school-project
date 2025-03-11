d1c3329ed87c01d5452ef5983284a28b474117f3
gissellenoriko481 <gissellenoriko481@gmail.com>
2025-03-11 08:46:59 +0000

    Add 6.cpp

---
+++ src/6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "John";
+    int age = 25;
+    cout << "My name is " << name << " and I am " << age << " years old." << endl;
+    return 0;
+}
