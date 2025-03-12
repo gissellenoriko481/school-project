946de3078aa4d846bb3402cf1815f911de84e304
gissellenoriko481 <gissellenoriko481@gmail.com>
2025-03-12 08:39:29 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 30;
+    if (age >= 18) {
+        cout << "You are eligible to vote." << endl;
+    } else {
+        cout << "You are not eligible to vote." << endl;
+    }
+    return 0;
+}
