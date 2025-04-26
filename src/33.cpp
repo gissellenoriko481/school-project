d383e1b2f540ba5e59cbd5ac6b984a80c15619b6
gissellenoriko481 <gissellenoriko481@gmail.com>
2025-04-26 11:55:12 +0000

    Add 33.cpp

---
+++ src/33.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 2.5;
+    char c = 'A';
+    cout << "Value of a: " << a << ", b: " << b << ", c: " << c << endl;
+    return 0;
+}
