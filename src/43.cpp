99837c2c91e6b65a2cef3e81571124a2bba0a8d7
gissellenoriko481 <gissellenoriko481@gmail.com>
2025-05-28 02:28:00 +0000

    Add 43.cpp

---
+++ src/43.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    std::cout << "x is " << x << std::endl;
+
+    double y = 3.14;
+    std::cout << "y is " << y << std::endl;
+
+    return 0;
+}
